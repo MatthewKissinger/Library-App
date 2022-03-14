ba328c0c7fff767299c95ae0e5631bcd68f56d78
Matt <matthew.t.kissinger@gmail.com>
2022-03-13 23:18:38 -0700

    setup basic styling and grid layout

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 console.log('Bookshelf App connected');
+
+
+// library array
+let myLibrary = [
+    {
+        title: 'The Pillars of the Earth',
+        author: 'Ken Follett',
+        pages: 806,
+        read: false
+    },
+    {
+        title: 'The Hobbit', 
+        author: 'J.R.R. Tolkien',
+        pages: 310,
+        read: true
+    }
+];
+
+// book constructor function
+function Book (title, author, pages, read) {
+    this.title = title, 
+    this.author = author, 
+    this.pages = pages, 
+    this.read = read
+}
+
+// methods
+
+function addBookToLibrary() {
+
+}
